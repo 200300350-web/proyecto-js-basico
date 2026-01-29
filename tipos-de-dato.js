829f70dc13137834d784b29ce8f8ba1305f995f8
Diego Rodriguez <200300350@ucaribe.edu.mx>
2026-01-29 11:36:05 -0500

    Mi tarea de javascript

---
+++ tipos-de-dato.js
@@ -0,0 +1,12 @@
+// tipos-de-dato.js
+console.log('42 es tipo:', typeof 42);
+console.log("'Veinticinco' es tipo:", typeof 'Veinticinco');
+console.log('-666 es tipo:', typeof -666);
+console.log('true es tipo:', typeof true);
+console.log('0 es tipo:', typeof 0);
+console.log("'' es tipo:", typeof '');
+console.log('null es tipo:', typeof null);
+console.log('undefined es tipo:', typeof undefined);
+console.log('FALSE (no definido) es tipo:', typeof FALSE); 
+// Tus ejemplos extras:
+console.log('3.1416 es tipo:', typeof 3.1416);
